5a3b66267e3ac8a62ad8d1daa4eb8ed78aab07ce
Brent Peterson <brentp24@gmail.com>
2020-05-18 17:07:53 -0600

    add dept, role, employee

--- server.js
+++ server.js
@@ -64,9 +64,9 @@ function viewDepartments() {
     connection.query("SELECT * FROM employee_tracker.departments;", function (err, res) {
         if (err) throw err;
         console.table(res);
-    })
+       
+});
 };
-
 
 function viewRoles() {
     connection.query("SELECT * FROM employee_tracker.roles;", function (err, res) {
@@ -90,9 +90,9 @@ function addDepartment() {
             message: "What is the name of the department?",
         })
         .then(function (response) {
-            connection.query ("INSERT INTO employee_tracker.departments (dept_name) VALUES (?)", [response.dept_name],function(err, res) {
-                if (err) throw err; 
-                console.table(res); 
+            connection.query("INSERT INTO employee_tracker.departments (dept_name) VALUES (?)", [response.dept_name], function (err, res) {
+                if (err) throw err;
+                console.table(res);
             })
         })
 };
@@ -100,6 +100,17 @@ function addDepartment() {
 
 
 function addRole() {
+    //get department names
+    var dept_choices = [];
+    var dept_ids = [];
+    connection.query("SELECT * FROM employee_tracker.departments;", function (err, res) {
+        if (err) throw err;
+        for (i = 0;i <res.length; i+=1){
+        dept_choices.push((res[i].dept_name))
+        dept_ids.push((res[i].dept_id));
+    }
+});
+
     inquirer.prompt([
         {
             name: "title",
@@ -115,18 +126,28 @@ function addRole() {
             name: "department",
             type: "list",
             message: "What is the department?",
-            choices: [1,2,3],
+            choices: dept_choices,
         }]
-        )
+    )
         .then(function (response) {
-            connection.query ("INSERT INTO employee_tracker.roles (title, salary, dept_id) VALUES(?,?,?)", [response.title, response.salary, response.department],function(err, res) {
-                if (err) throw err; 
-                console.table(res); 
+            connection.query("INSERT INTO employee_tracker.roles (title, salary, dept_id) VALUES(?,?,?)", [response.title, response.salary, dept_ids[dept_choices.indexOf(response.department)]], function (err, res) {
+                if (err) throw err;
+                console.table(res);
             })
         })
-    }
+}
 
 function addEmployee() {
+        //get role names
+        var role_choices = [];
+        var role_ids = [];
+        connection.query("SELECT * FROM employee_tracker.roles;", function (err, res) {
+            if (err) throw err;
+            for (i = 0;i <res.length; i+=1){
+            role_choices.push((res[i].title))
+            role_ids.push((res[i].role_id));
+        }
+    });
     inquirer.prompt([
         {
             name: "first_name",
@@ -139,23 +160,23 @@ function addEmployee() {
             message: "What is the last name?",
         },
         {
-            name: "role_id",
+            name: "title",
             type: "list",
             message: "What is the role?",
-            choices: [1,2,3],
+            choices: role_choices,
         },
         {
             name: "manager_id",
             type: "list",
             message: "Who is the manager?",
-            choices: [1,2,3],
+            choices: [1, 2, 3],
         }
     ]
-        )
+    )
         .then(function (response) {
-            connection.query ("INSERT INTO employee_tracker.employees (first_name, last_name, role_id, manager_id) VALUES(?,?,?,?)", [response.first_name, response.last_name, response.role_id, response.manager_id],function(err, res) {
-                if (err) throw err; 
-                console.table(res); 
+            connection.query("INSERT INTO employee_tracker.employees (first_name, last_name, role_id, manager_id) VALUES(?,?,?,?)", [response.first_name, response.last_name, role_ids[role_choices.indexOf(response.title)], response.manager_id], function (err, res) {
+                if (err) throw err;
+                console.table(res);
             })
         })
 }
